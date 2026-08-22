3cc0abe2d8da4515168bd8a08787cb6bf989acd3
i <isomorphisms@sdf.org>
2026-08-22 17:16:17 -0400

    Export storage inspector API

--- ib/__init__.py
+++ ib/__init__.py
@@ -1,10 +1,14 @@
 from .history import HistoryEntry, available_history_adapters, ingest_history, register_history_adapter
 from .indexes import HistoryIndices, IndexBuilder, write_plaintext_indices
+from .inspect import StorageInspector, StoreFile, TabRecord
 
 __all__ = [
     "HistoryEntry",
     "HistoryIndices",
     "IndexBuilder",
+    "StorageInspector",
+    "StoreFile",
+    "TabRecord",
     "available_history_adapters",
     "ingest_history",
     "register_history_adapter",
