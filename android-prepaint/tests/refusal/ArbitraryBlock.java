c7f5ea96e260e8ff4f0f774fd0d3a60752a6f8bb
i <isomorphisms@sdf.org>
2026-09-07 15:09:19 -0400

    Preserve typed prepaint block variants

---
+++ android-prepaint/tests/refusal/ArbitraryBlock.java
@@ -0,0 +1,6 @@
+package org.isomorphisms.ib.prepaint;
+
+final class ArbitraryBlock {
+    PrepaintDocument.Block invalid =
+            PrepaintDocument.Block.of("image", "only one untyped field");
+}
