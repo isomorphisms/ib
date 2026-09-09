562ce77495d8caffcdff67295b9a808594dddfb4
i <isomorphisms@sdf.org>
2026-09-08 23:03:10 -0400

    Stub Material 3 arXiv viewer

---
+++ android-material3/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ib-material3-arxiv-viewer"
+include(":app")
