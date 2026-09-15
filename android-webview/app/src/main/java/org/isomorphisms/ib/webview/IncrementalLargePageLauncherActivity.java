a8ecb5b3e4c75a2f5682ea5b82ed262b89935457
i <isomorphisms@sdf.org>
2026-09-15 19:58:38 -0400

    Add tap-launcher for incremental page experiment

---
+++ android-webview/app/src/main/java/org/isomorphisms/ib/webview/IncrementalLargePageLauncherActivity.java
@@ -0,0 +1,25 @@
+package org.isomorphisms.ib.webview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public final class IncrementalLargePageLauncherActivity extends Activity {
+    private static final String TARGET_URL =
+        "https://console.cloud.google.com/agent-platform/studio/multimodal"
+            + "?authuser=5"
+            + "&project=isomorphismes-youtube-shorts"
+            + "&supportedpurview=project"
+            + "&model=gemini-3.7-flash"
+            + "&region=global";
+
+    @Override
+    protected void onCreate(Bundle saved_instance_state) {
+        super.onCreate(saved_instance_state);
+
+        Intent intent = new Intent(this, IncrementalLargePageActivity.class);
+        intent.putExtra("url", TARGET_URL);
+        startActivity(intent);
+        finish();
+    }
+}
